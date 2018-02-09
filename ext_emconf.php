1eb4e92f0954f1db041b45e4ca2dc79c0a388c03
Jeffrey P.A. de Bruijn <jeffrey.de.bruijn@gmail.com>
2018-02-09 09:40:12 -0500

    dependency file edited

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,14 +12,13 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '8.7.0-9.5.99',
             'fluid_styled_content' => '8.7.0-9.5.99',
             'rte_ckeditor' => '8.7.0-9.5.99',
-	    'gridelements' => '8.7.0-9.5.99',
-	    'bootstrap_grids' => '8.7.0-9.5.99',
-	    'news' => '8.7.0-9.5.99',
-	    'cs_seo' => '8.7.0-9.5.99',
-            'form_legacy' => '8.7.0-9.5.99',
-            'ke_search' => '8.7.0-9.5.99',
-            'form_legacy' => '8.7.0-9.5.99',
-            'realurl' => '8.7.0-9.5.99'
+	    'gridelements' => '8.0.0-0.0.0',
+	    'bootstrap_grids' => '1.4.0-0.0.0',
+	    'news' => '6.3.0-0.0.0',
+	    'cs_seo' => '2.2.0-0.0.0',
+            'form_legacy' => '0.0.0-0.0.0',
+            'ke_search' => '2.6.3-0.0.0',
+            'realurl' => '2.3.1-0.0.0'
         ],
         'conflicts' => [
         ],
