8c658b59e65185eee53481f906c4b104b6235a93
Jeffrey P.A. de Bruijn <jeffrey.de.bruijn@gmail.com>
2018-02-09 09:21:03 -0500

    dependecies added

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,15 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
             'fluid_styled_content' => '8.7.0-9.5.99',
-            'rte_ckeditor' => '8.7.0-9.5.99'
+            'rte_ckeditor' => '8.7.0-9.5.99',
+	    'gridelements' => '8.7.0-9.5.99',
+	    'bootstrap_grids' => '8.7.0-9.5.99',
+	    'news' => '8.7.0-9.5.99',
+	    'cs_seo' => '8.7.0-9.5.99',
+            'form_legacy' => '8.7.0-9.5.99',
+            'ke_search' => '8.7.0-9.5.99',
+            'form_legacy' => '8.7.0-9.5.99',
+            'realurl' => '8.7.0-9.5.99'
         ],
         'conflicts' => [
         ],
